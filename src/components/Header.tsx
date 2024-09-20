06267761b7361377a5070cef32bbe136deeb4293
Luciano Ramello <luchosr@gmail.com>
2024-09-20 11:43:11 +0200

    refactor. implements NavLink component for conditional rendering of navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="bg-slate-800">
@@ -7,7 +8,28 @@ export default function Header() {
             <img src="/logo.svg" alt="logotipo" className="w-32" />
           </div>
 
-          <nav></nav>
+          <nav className="flex gap-4">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? 'uppercase font-bold text-orange-500'
+                  : 'uppercase font-bold text-white'
+              }
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to="/favoritos"
+              className={({ isActive }) =>
+                isActive
+                  ? 'uppercase font-bold text-orange-500'
+                  : 'uppercase font-bold text-white'
+              }
+            >
+              Favoritos
+            </NavLink>
+          </nav>
         </div>
       </div>
     </header>
