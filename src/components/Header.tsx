965a43a3f3866f72f90964d2dc3853c861a1efa7
Luciano Ramello <luchosr@gmail.com>
2024-09-20 12:24:01 +0200

    feat: adds form conditional rendering on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import { NavLink } from 'react-router-dom';
+import { useMemo } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 export default function Header() {
+  const { pathname } = useLocation();
+  const isHome = useMemo(() => pathname === '/', [pathname]);
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -31,6 +34,45 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o Ingredientes
+              </label>
+              <input
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Ingrese el Nombre o Ingredientes de su bebida"
+              />
+            </div>
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoría
+              </label>
+              <select
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+              >
+                <option value=""> -- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Buscar Recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
