bab9b2cd53d3f493e161016b823d6e6923ba92ea
Luciano Ramello <luchosr@gmail.com>
2024-09-20 11:28:14 +0200

    feat: implements layout component on router

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Indexpage from './views/Indexpage';
 import FavoritesPage from './views/FavoritesPage';
+import Layout from './layouts/Layout';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Indexpage />} />
-        <Route path="/favoritos" element={<FavoritesPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Indexpage />} index />
+          <Route path="/favoritos" element={<FavoritesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
