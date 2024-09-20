770f7173e3abdcafb206f23c0f33e9d78b1825ee
Luciano Ramello <luchosr@gmail.com>
2024-09-20 15:01:56 +0200

    feat: creates recipes store slice

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,10 @@
+import { StateCreator } from 'zustand';
+
+type Category = object;
+export type RecipesSliceType = {
+  categories: Category[];
+};
+
+export const createRecipesSlice: StateCreator<RecipesSliceType> = () => ({
+  categories: [],
+});
