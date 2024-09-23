7f429b74215f2ec1624a291a07a1d38e53188f2f
Luciano Ramello <luchosr@gmail.com>
2024-09-23 18:32:11 +0200

    feat: creates search filter type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
+import { searchFilterSchema } from './../utils/recipes-schema';
 import { z } from 'zod';
 import { CategoriesAPIResponseSchema } from '../utils/recipes-schema';
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
+export type searchFilter = z.infer<typeof searchFilterSchema>;
