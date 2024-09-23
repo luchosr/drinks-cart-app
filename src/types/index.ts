42b4bc608cfff218a516019ee05358a26b4d787a
Luciano Ramello <luchosr@gmail.com>
2024-09-23 18:59:34 +0200

    feat: creates Drink type from schemas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,10 @@
-import { searchFilterSchema } from './../utils/recipes-schema';
+import {
+  DrinksAPIResponse,
+  searchFilterSchema,
+} from './../utils/recipes-schema';
 import { z } from 'zod';
 import { CategoriesAPIResponseSchema } from '../utils/recipes-schema';
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
-export type searchFilter = z.infer<typeof searchFilterSchema>;
+export type SearchFilter = z.infer<typeof searchFilterSchema>;
+export type Drinks = z.infer<typeof DrinksAPIResponse>;
