77630da10575d7cc5430ec695530b563f4f5cd57
Luciano Ramello <luchosr@gmail.com>
2024-09-23 11:30:11 +0200

    feat: implements zustand devtools

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,10 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, RecipesSliceType } from './recipeSlice';
 
 // aqui se pasa ...a refiriendose a los argumentos set, get, api del store de zustand
-export const useAppStore = create<RecipesSliceType>((...a) => ({
-  ...createRecipesSlice(...a),
-}));
+export const useAppStore = create<RecipesSliceType>()(
+  devtools((...a) => ({
+    ...createRecipesSlice(...a),
+  }))
+);
