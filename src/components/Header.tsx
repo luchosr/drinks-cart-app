19f3d495e2e7c3d764764e72169ada1d81ba1e53
Luciano Ramello <luchosr@gmail.com>
2024-09-23 18:32:59 +0200

    feat: implements search recipes function from app store

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useAppStore } from '../stores/useAppStore';
+
 export default function Header() {
   const [searchFilters, setSearchFilters] = useState({
     ingredient: '',
@@ -11,6 +12,7 @@ export default function Header() {
 
   const fetchCategories = useAppStore((state) => state.fetchCategories);
   const { drinks } = useAppStore((state) => state.categories);
+  const searchRecipes = useAppStore((state) => state.searchRecipes);
 
   useEffect(() => {
     fetchCategories();
@@ -26,6 +28,18 @@ export default function Header() {
         ...searchFilters,
         [e.target.name]: e.target.value,
       });
+    },
+    [searchFilters]
+  );
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (Object.values(searchFilters).includes('')) {
+        console.log('Todos los campos son obligatorios');
+        return;
+      }
+      searchRecipes(searchFilters);
     },
     [searchFilters]
   );
@@ -64,7 +78,10 @@ export default function Header() {
           </nav>
         </div>
         {isHome && (
-          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+          <form
+            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+            onSubmit={handleSubmit}
+          >
             <div className="space-y-4">
               <label
                 htmlFor="ingredient"
