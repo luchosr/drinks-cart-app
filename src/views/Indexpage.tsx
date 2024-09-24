613349a53aaec0adc712e6a3890c801a7a081678
Luciano Ramello <luchosr@gmail.com>
2024-09-24 16:40:03 +0200

    style: adds tailwinds classes to index page structure

--- src/views/Indexpage.tsx
+++ src/views/Indexpage.tsx
@@ -10,11 +10,11 @@ export default function Indexpage() {
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
           {drinks.drinks.map((drink) => (
             <DrinkCard key={drink.idDrink} drink={drink} />
           ))}
-        </>
+        </div>
       ) : (
         <p className="my-10 text-center text-2xl">
           No hay resultados ahún, utiliza el formulario par buscar recetas
