d2f3a24724d860a1ecec38c95847bfae0454d23d
Luciano Ramello <luchosr@gmail.com>
2024-09-24 19:11:34 +0200

    feat: creates get recipe by id fetching service

--- src/services/recipesServices.ts
+++ src/services/recipesServices.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import {
   CategoriesAPIResponseSchema,
   DrinksAPIResponse,
+  RecipeAPIResponseSchema,
 } from '../utils/recipes-schema';
-import { SearchFilter } from '../types';
+import { Drink, SearchFilter } from '../types';
 
 export async function getCategories() {
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
@@ -19,6 +20,18 @@ export async function getRecipes(filters: SearchFilter) {
 
   const { data } = await axios(url);
   const result = DrinksAPIResponse.safeParse(data);
+  if (result.success) {
+    return result.data;
+  }
+}
+
+export async function getRecipeById(id: Drink['idDrink']) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  const { data } = await axios(url);
+
+  const result = RecipeAPIResponseSchema.safeParse(data.drinks[0]);
+
   if (result.success) {
     return result.data;
   }
