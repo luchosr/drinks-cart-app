48d659cdde64995f10f06f1af43adeaa6b2767fb
Luciano Ramello <luchosr@gmail.com>
2024-09-25 16:32:44 +0200

    feat: implements Favorite slice on app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, RecipesSliceType } from './recipeSlice';
+import { createFavoritesSlice, FavoritesSliceType } from './favoritesSlice';
 
 // aqui se pasa ...a refiriendose a los argumentos set, get, api del store de zustand
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
