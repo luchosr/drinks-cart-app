cc065f7680eee09422ab98b3faa510982543737a
Luciano Ramello <luchosr@gmail.com>
2024-09-25 16:31:26 +0200

    feat: adds modal's action buttons

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -83,6 +83,21 @@ export default function Modal() {
                   <p className="text-gray-700 text-lg">
                     {selectedRecipe.strInstructions}
                   </p>
+                  <div className="mt-5 flex justify-between gap-4">
+                    <button
+                      type="button"
+                      className="w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500"
+                      onClick={closeModal}
+                    >
+                      Cerrar
+                    </button>
+                    <button
+                      type="button"
+                      className="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
+                    >
+                      Agregar a favoritos
+                    </button>
+                  </div>
                 </DialogPanel>
               </TransitionChild>
             </div>
