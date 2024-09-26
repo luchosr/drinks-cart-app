5ea95b9e05c9ed63530397113fcbd751cdf68893
Luciano Ramello <luchosr@gmail.com>
2024-09-26 11:58:56 +0200

    feat: implements favorites page conditional rendering

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,3 +1,25 @@
+import { useMemo } from 'react';
+import DrinkCard from '../components/DrinkCard';
+import { useAppStore } from '../stores/useAppStore';
+
 export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const favorites = useAppStore((state) => state.favorites);
+
+  const hasFavorites = useMemo(() => favorites.length, [favorites]);
+  return (
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
+          {favorites.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          No hay favoritos por el momento, agrege el que mejor le parezca.
+        </p>
+      )}
+    </>
+  );
 }
