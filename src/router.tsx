4da3f1ffeb8ea22ce4d353d6ce60400f4b22c6cc
Luciano Ramello <luchosr@gmail.com>
2024-09-27 00:16:40 +0200

    refactor: adds Index page with lazy loading

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,24 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Indexpage from './views/Indexpage';
+
 import Layout from './layouts/Layout';
 
+const Indexpage = lazy(() => import('./views/Indexpage'));
 const FavoritesPage = lazy(() => import('./views/FavoritesPage'));
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Indexpage />} index />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<p>Cargando...</p>}>
+                <Indexpage />
+              </Suspense>
+            }
+            index
+          />
           <Route
             path="/favoritos"
             element={
