a183f646df65624a5028dee54b6224f86171cfa3
Luciano Ramello <luchosr@gmail.com>
2024-09-26 10:28:46 +0200

    feat: implements handleClickFavorite on modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,6 +13,7 @@ export default function Modal() {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+  const handleClickFavorite = useAppStore((state) => state.handleClickFavorite);
   const renderIngredients = () => {
     const ingredients: JSX.Element[] = [];
     for (let i = 1; i <= 6; i++) {
@@ -94,6 +95,7 @@ export default function Modal() {
                     <button
                       type="button"
                       className="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
+                      onClick={() => handleClickFavorite(selectedRecipe)}
                     >
                       Agregar a favoritos
                     </button>
