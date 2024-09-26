990122457c2cb094d2ef05c90bcc73b9e84ed5f1
Luciano Ramello <luchosr@gmail.com>
2024-09-26 18:33:37 +0200

    feat: improves performance using lazy loading and react  suspense.

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,23 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Indexpage from './views/Indexpage';
-import FavoritesPage from './views/FavoritesPage';
 import Layout from './layouts/Layout';
 
+const FavoritesPage = lazy(() => import('./views/FavoritesPage'));
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Indexpage />} index />
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route
+            path="/favoritos"
+            element={
+              <Suspense fallback={<p>Cargando...</p>}>
+                <FavoritesPage />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
