5de4489a11aa695e6e3a10d85452e2057fe75648
Luciano Ramello <luchosr@gmail.com>
2024-09-26 10:28:21 +0200

    feat: implements favorite handler on favorites Slice

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -3,8 +3,22 @@ import { Recipe } from '../types';
 
 export type FavoritesSliceType = {
   favorites: Recipe[];
+  handleClickFavorite: (recipe: Recipe) => void;
 };
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (
+  set,
+  get
+) => ({
   favorites: [],
+  handleClickFavorite: (recipe) => {
+    if (
+      get().favorites.some((favorite) => favorite.idDrink === recipe.idDrink)
+    ) {
+      console.log('Recipe already added');
+    } else {
+      console.log('Recipe not  added');
+      set((state) => ({ favorites: [...state.favorites, recipe] }));
+    }
+  },
 });
