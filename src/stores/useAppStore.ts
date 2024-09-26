f0573263d9ccd1b13417b57b6299f3d2befea645
Luciano Ramello <luchosr@gmail.com>
2024-09-26 12:34:18 +0200

    feat: adds notification slice to app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,11 +2,18 @@ import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, RecipesSliceType } from './recipeSlice';
 import { createFavoritesSlice, FavoritesSliceType } from './favoritesSlice';
+import {
+  createNotificationSlice,
+  NotificationSliceType,
+} from './NotificationSlice';
 
 // aqui se pasa ...a refiriendose a los argumentos set, get, api del store de zustand
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
+export const useAppStore = create<
+  RecipesSliceType & FavoritesSliceType & NotificationSliceType
+>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
   }))
 );
