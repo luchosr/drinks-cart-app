e833f47b015d52309cb7e22a712af535ea467315
Luciano Ramello <luchosr@gmail.com>
2024-11-01 15:54:15 +0100

    test: updates Favorites Pages tests

--- src/__tests__/FavoritesPage.unit.test.tsx
+++ src/__tests__/FavoritesPage.unit.test.tsx
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import FavoritesPage from '../views/FavoritesPage';
 
-test('renders without error', () => {
-  render(<FavoritesPage />);
-});
+describe('FavoritesPage', () => {
+  beforeEach(() => {
+    render(<FavoritesPage />);
+  });
 
-test('renders content', () => {
-  render(<FavoritesPage />);
+  test('renders main  content title', () => {
+    const element = screen.getByText('Favoritos');
+    expect(element).toBeDefined();
+  });
 
-  const element = screen.getByText('Favoritos');
-  expect(element).toBeDefined();
+  test('shows fallback content when there are no favorites', () => {
+    const element = screen.getByText(
+      'No hay favoritos por el momento, agrege el que mejor le parezca.'
+    );
+    expect(element).toBeDefined();
+  });
 });
