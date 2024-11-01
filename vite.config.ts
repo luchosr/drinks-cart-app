7ad77e162bcb4985dacb5f0bff3ee133600bd47e
Luciano Ramello <luchosr@gmail.com>
2024-11-01 15:16:46 +0100

    test: adds vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.ts',
+  },
+});
