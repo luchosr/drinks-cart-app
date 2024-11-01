dfe555a1a16d02ca7dd64088a268528d15d88835
Luciano Ramello <luchosr@gmail.com>
2024-11-01 15:53:54 +0100

    test: updates Index Page tests

--- src/__tests__/Indexpage.unit.test.tsx
+++ src/__tests__/Indexpage.unit.test.tsx
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Indexpage from '../views/Indexpage';
 
-test('renders without error', () => {
-  render(<Indexpage />);
-});
+describe('Indexpage', () => {
+  beforeEach(() => {
+    render(<Indexpage />);
+  });
 
-test('renders content', () => {
-  render(<Indexpage />);
+  test('renders content', () => {
+    const element = screen.getByText('Recetas');
+    expect(element).toBeDefined();
+  });
 
-  const element = screen.getByText('Recetas');
-  expect(element).toBeDefined();
+  test('shows fallback content when there are no favorites', () => {
+    const element = screen.getByText(
+      'No hay resultados por el momento, utiliza el formulario par buscar recetas'
+    );
+    expect(element).toBeDefined();
+  });
 });
